295443031988d4264147a66a796be7bb5e905f63
Tomasz Pęczek <tpeczek@gmail.com>
2026-01-05 20:09:36 +0100

    Upgrade to .NET 10

--- Demo.AspNetCore.WebSockets/Program.cs
+++ Demo.AspNetCore.WebSockets/Program.cs
@@ -1,17 +1,42 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Demo.AspNetCore.WebSockets.Services;
+using Demo.AspNetCore.WebSockets.Middlewares;
+using Demo.AspNetCore.WebSockets.Infrastructure;
 
-namespace Demo.AspNetCore.WebSockets
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddWebSocketConnections();
+
+builder.Services.AddSingleton<IHostedService, HeartbeatService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-        
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+DefaultFilesOptions defaultFilesOptions = new();
+defaultFilesOptions.DefaultFileNames.Clear();
+defaultFilesOptions.DefaultFileNames.Add("websocket-api.html");
+
+ITextWebSocketSubprotocol textWebSocketSubprotocol = new PlainTextWebSocketSubprotocol();
+WebSocketConnectionsOptions webSocketConnectionsOptions = new()
+{
+    AllowedOrigins = ["http://localhost:63290"],
+    SupportedSubProtocols = [new JsonWebSocketSubprotocol(), textWebSocketSubprotocol],
+    DefaultSubProtocol = textWebSocketSubprotocol,
+    SendSegmentSize = 4 * 1024
+};
+
+app.UseDefaultFiles(defaultFilesOptions);
+app.UseStaticFiles();
+
+app.UseWebSockets();
+app.MapWebSocketConnections("/socket", webSocketConnectionsOptions);
+
+app.Run();
